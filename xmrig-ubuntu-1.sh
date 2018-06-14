d8eb5929d19805ee0adbe465f44a736bdc2af27e
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-14 16:49:29 -0700

    Update xmrig-ubuntu-1.sh

--- xmrig-ubuntu-1.sh
+++ xmrig-ubuntu-1.sh
@@ -1,1 +1,10 @@
+sudo apt-get -y update
+sudo apt-get install -y epel-release tmux
+sudo apt-get install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
+make
+expect ./expect-2.sh
+./xmrig -o qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x -k --donate-level=1
 
