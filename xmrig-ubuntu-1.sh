a97642fcd6eaf34e746f23b66accee5a67e5fa40
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-19 14:04:54 -0700

    Update xmrig-ubuntu-1.sh

--- xmrig-ubuntu-1.sh
+++ xmrig-ubuntu-1.sh
@@ -5,6 +5,7 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
 make
+mv ~/xmrig-autoscript/expect-2.sh ~/xmrig-autoscript/xmrig/build
 expect ./expect-2.sh
 ./xmrig -o qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x -k --donate-level=1
 
