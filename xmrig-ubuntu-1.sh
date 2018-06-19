49aa78c453a8cac0d636e0e67d82a9a15477d467
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-19 14:26:02 -0700

    Update xmrig-ubuntu-1.sh

--- xmrig-ubuntu-1.sh
+++ xmrig-ubuntu-1.sh
@@ -7,5 +7,7 @@ mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr
 make
 mv ~/xmrig-autoscript/expect-2.sh ~/xmrig-autoscript/xmrig/build
 expect ./expect-2.sh
+tmux
+trap "" 15
 ./xmrig -o qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x -k --donate-level=1
 
