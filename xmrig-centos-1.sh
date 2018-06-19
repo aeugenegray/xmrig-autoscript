2bd8e0e65474ce3173a609f1190bcd3c8f755ad0
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-19 13:51:30 -0700

    Update xmrig-centos-1.sh

--- xmrig-centos-1.sh
+++ xmrig-centos-1.sh
@@ -1,10 +1,10 @@
-sudo yum -y update
 sudo yum install -y epel-release tmux
 sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
 make
+mv ~/xmrig-autoscript/expect-2.sh ~/xmrig-autoscript/xmr-stak/build
 expect ./expect-2.sh
 ./xmrig --algo=cryptonight-lite --variant 1 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd -p x -k --donate-level=1
 
