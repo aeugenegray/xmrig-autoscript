7896976117e20229b5e5ad07c4332281bc7c1c46
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-25 02:53:41 -0700

    Update xmrig-centos-1.sh

--- xmrig-centos-1.sh
+++ xmrig-centos-1.sh
@@ -14,4 +14,4 @@ tmux
 # Kill sigterm
 trap "" 15
 # Final Script Run -- **CHANGE ALGO, POOL AND USER BELOW**
-./xmrig --algo=cryptonight-lite --variant 1 -o xat.mineallcrypto.com:7656 -u XA4tS3ffGZR6AqiygDZptz8nc93Jm6Xsr9gwBya6REyH6eC4FTz4kQRHiLXt7gtUcAB35k1Bodvnf6b7C4FP7HM533bn1w9fk -p x -k --donate-level=1
+./xmrig --currency cryptonight_v7 -o xat.mineallcrypto.com:7656 -u XA4tS3ffGZR6AqiygDZptz8nc93Jm6Xsr9gwBya6REyH6eC4FTz4kQRHiLXt7gtUcAB35k1Bodvnf6b7C4FP7HM533bn1w9fk -p x -k --donate-level=1
