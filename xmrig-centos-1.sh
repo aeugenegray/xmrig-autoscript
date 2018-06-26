25beb18bf580ac8337c77599848f879de0b69556
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-25 23:30:45 -0700

    Update xmrig-centos-1.sh

--- xmrig-centos-1.sh
+++ xmrig-centos-1.sh
@@ -15,3 +15,4 @@ tmux
 trap "" 15
 # Final Script Run -- **CHANGE ALGO, POOL AND USER BELOW**
 ./xmrig --variant 1 -o xat.mineallcrypto.com:7656 -u XA4tS3ffGZR6AqiygDZptz8nc93Jm6Xsr9gwBya6REyH6eC4FTz4kQRHiLXt7gtUcAB35k1Bodvnf6b7C4FP7HM533bn1w9fk -p x -k --donate-level=1
+
