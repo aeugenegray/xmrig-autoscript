b5e2d8372194bdb9246c4c323a61ca4c8550f032
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-26 15:05:30 -0700

    Create xmrig-centos-cn-v7.sh

---
+++ xmrig-centos-cn-v7.sh
@@ -0,0 +1,17 @@
+# Installs Dependencies 
+sudo yum install -y epel-release tmux
+sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
+# Downloads Package and Installs
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
+make
+# Moves and Runs Expect Script. 
+mv ~/xmrig-autoscript/expect-2.sh ~/xmrig-autoscript/xmrig/build
+expect ./expect-2.sh
+# Runs tmux to keep a session open even after closing
+tmux
+# Kill sigterm
+trap "" 15
+# Final Script Run -- **CHANGE ALGO, POOL AND USER BELOW**
+./xmrig --variant 1 -o xat.mineallcrypto.com:7656 -u XA4tS3ffGZR6AqiygDZptz8nc93Jm6Xsr9gwBya6REyH6eC4FTz4kQRHiLXt7gtUcAB35k1Bodvnf6b7C4FP7HM533bn1w9fk -p x -k --donate-level=1
