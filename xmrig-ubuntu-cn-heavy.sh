412716d2722f6cf6f70f313545e668e437c7b00c
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-29 07:42:35 +0000

    Update xmrig-ubuntu-cn-heavy.sh

--- xmrig-ubuntu-cn-heavy.sh
+++ xmrig-ubuntu-cn-heavy.sh
@@ -14,4 +14,4 @@ tmux
 # Kill sigterm
 trap "" 15
 # Final Script Run -- **CHANGE ALGO, POOL AND USER BELOW**
-./xmrig --variant 1 -o xat.mineallcrypto.com:7656 -u XA4tS3ffGZR6AqiygDZptz8nc93Jm6Xsr9gwBya6REyH6eC4FTz4kQRHiLXt7gtUcAB35k1Bodvnf6b7C4FP7HM533bn1w9fk -p x -k --donate-level=1
+./xmrig --algo=cryptonight-heavy -o bxb.mineallcrypto.com:7686 -u bxcjGe8gYig5V9f5a7tmQUXxYw2owbvtLWmfv3KVNLHUUS1Sdxfr4R9SpKWX1kdYTVCq34wDkne1KBAcjzmwSuV21hgdYDe4h -p x -k --donate-level=1
