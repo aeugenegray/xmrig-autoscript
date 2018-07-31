8e5f635207457ec63a761c410909a0e3b2c5a0ca
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-07-31 06:49:45 +0000

    Update expect-2.sh

--- expect-2.sh
+++ expect-2.sh
@@ -1,4 +1,4 @@
-spawn ./xmrig --algo=cryptonight-lite --variant 1 -o spot.mineallcrypto.com:7586 -u Ad4CkyGhRfLWvLbj81zM4yD9CsCu9n15o9y5w77GhLKXfZ89wm9BXcEKnW835ruUVvBvTjwhcfaDufmBrvjzCYY6HjziHwr -p x -k --donate-level=1
+spawn /xmrig --algo=cryptonight-lite --variant 1 -o lch-us.walpool.com:5088 -u cashFZEviaRfxNFBkbcZm45emNqk26Nb942Fn6NnrCAwRin8aEQTuAceKyTxtsWkumYWphYMK3kSkDhA6APysyMe6HFMkjDFhP -p x -k --donate-level=1
 expect -re "#\r"
 send -- "0\r"
 set timeout 120
