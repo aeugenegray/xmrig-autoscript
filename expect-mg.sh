3067e5f2024762d4a848024767d3479eb67349d1
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-04 02:31:53 +0000

    Update expect-mg.sh

--- expect-mg.sh
+++ expect-mg.sh
@@ -1,5 +1,5 @@
 spawn ./xmrig --algo=cryptonight-lite --variant 1 -o lch-us.walpool.com:5088 -u cashFZEviaRfxNFBkbcZm45emNqk26Nb942Fn6NnrCAwRin8aEQTuAceKyTxtsWkumYWphYMK3kSkDhA6APysyMe6HFMkjDFhP -p x -k --donate-level=1
 expect -re "#\r"
 send -- "0\r"
-set timeout 120
+set timeout 1200000
 expect eof
