a6ceab86d49e73c312b39bb32b6860b12cb8e8d8
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-04 02:50:46 +0000

    Update expect-mg.sh

--- expect-mg.sh
+++ expect-mg.sh
@@ -1,4 +1,4 @@
-spawn ./xmrig --variant 1 -o pool.goro.info:3333 -u goroCoinVN6hYPfMr3vTdHi18RV3CzdryFCQbpeHwNYYZYoDMQfFVtm7VWtzgM5vahhiZzrYcK41EA7ktahb7P9mZEmXtVzTpmg3GT -p x -k --donate-level=1
+spawn ./xmrig --variant 1 -o goro.clevery.xyz:10101 -u goroCoinVN6hYPfMr3vTdHi18RV3CzdryFCQbpeHwNYYZYoDMQfFVtm7VWtzgM5vahhiZzrYcK41EA7ktahb7P9mZEmXtVzTpmg3GT -p x -k --donate-level=1
 expect -re "#\r"
 send -- "0\r"
 set timeout 1200000
