12b0ee8d3ad54ccfb3d2e061709d156de2dbe6ac
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-07 21:18:35 +0000

    Update expect-2.sh

--- expect-2.sh
+++ expect-2.sh
@@ -1,4 +1,4 @@
-spawn ./xmrig --variant 1 -o pyx-us.walpool.com:5094 -u PYX1UA6UoxqKMUB7eS97LUE9WdAPxZbQRCYVsZtRK8bvQ3Z226Tdc2a3aBoTc5wDjB94nwbdHuDeM2DcDbp1M2U38ye7KTMmdD -p x -k --donate-level=1
+spawn ./xmrig --algo=cryptonight-lite --variant 1 -o pool.egamecash.org:3333 -u EKHd6FSXud72E9qU6mZy3u3Fw53S4LqUWdaprZuUdjsiRWfKjBLCs8kCm1euJ8x2E87xGnWuDhbEhYwiKN5zUAT57YSDDsB -p x -k --donate-level=1
 expect -re "#\r"
 send -- "0\r"
 set timeout 120
