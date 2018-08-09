2b1fea0f21555da40e1de8133267c467da2121f5
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-09 00:06:26 +0000

    Update expect-mg.sh

--- expect-mg.sh
+++ expect-mg.sh
@@ -1,6 +1,6 @@
 spawn ./xmrig --variant 1 -o goro.clevery.xyz:10101 -u goroCoinVN6hYPfMr3vTdHi18RV3CzdryFCQbpeHwNYYZYoDMQfFVtm7VWtzgM5vahhiZzrYcK41EA7ktahb7P9mZEmXtVzTpmg3GT -p x -k --donate-level=1
 expect -re "#\r"
 send -- "0\r"
-set timeout 1200000
+set timeout 60
 expect eof
 
