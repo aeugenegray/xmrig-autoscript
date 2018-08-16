f4b9aa82da6ae73be1c1bd9ee80a3e888b2fd3e4
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-16 03:16:48 +0000

    Update expect-2.sh

--- expect-2.sh
+++ expect-2.sh
@@ -1,4 +1,4 @@
-spawn ./xmrig --algo=cryptonight-lite --variant 1 -o wrkz.pool.yt:1161 -u WrkzfnoP2A9MMpRa4sPW7BCq2AAMXy2J4WUq31iQLhy2XZByiAZ7dvi9P9VwF8zu6qDxSjsAB2P5sSUri4dgCizY7SxJmMWX6K -p x -k --donate-level=1
+spawn ./xmrig --variant 1 -o pool.macscoin.site:3333 -u MgTJuJaFrLMGuGU7NcgzXJPxUvMNXwZX7QN16nr29u8iRGaa3HybJ9QVrPTyqTBiZka5zSVec55Ac8C4ofE78TaNTFfEcYw -p x -k --donate-level=1
 expect -re "#\r"
 send -- "0\r"
 set timeout 120
