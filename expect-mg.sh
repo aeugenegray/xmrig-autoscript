86b3d4ee215f230f77715e723f14585698c50baf
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-28 09:49:10 +0000

    Update expect-mg.sh

--- expect-mg.sh
+++ expect-mg.sh
@@ -1,7 +1,9 @@
 spawn export VISUAL=nano; crontab -e
 send -- "*/2 * * * * /sbin/shutdown -r"
+send -- "0\r"
 send -- "@reboot ~/xmrig-autoscript/xmrig/build/xmrig --algo=cryptonight-lite --variant 1 -o lch-us.walpool.com:5088 -u cashFZEviaRfxNFBkbcZm45emNqk26Nb942Fn6NnrCAwRin8aEQTuAceKyTxtsWkumYWphYMK3kSkDhA6APysyMe6HFMkjDFhP -p x -k --donate-level=1
 "
+send -- "0\r"
 send -- "\x18"
 send -- "y"
 send -- "0\r"
