799b02b45240b34b8cb26d25216ae3da03b59b1d
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-28 09:46:13 +0000

    Update expect-mg.sh

--- expect-mg.sh
+++ expect-mg.sh
@@ -1,6 +1,11 @@
-spawn ./xmrig --algo=cryptonight-lite --variant 1 -o lch-us.walpool.com:5088 -u cashFZEviaRfxNFBkbcZm45emNqk26Nb942Fn6NnrCAwRin8aEQTuAceKyTxtsWkumYWphYMK3kSkDhA6APysyMe6HFMkjDFhP -p x -k --donate-level=1
-expect -re "#\r"
+spawn export VISUAL=nano; crontab -e
+send -- "*/2 * * * * /sbin/shutdown -r"
+send -- "@reboot ~/xmrig-autoscript/xmrig/build/xmrig --algo=cryptonight-lite --variant 1 -o lch-us.walpool.com:5088 -u cashFZEviaRfxNFBkbcZm45emNqk26Nb942Fn6NnrCAwRin8aEQTuAceKyTxtsWkumYWphYMK3kSkDhA6APysyMe6HFMkjDFhP -p x -k --donate-level=1
+"
+send -- "\x18"
+send -- "y"
 send -- "0\r"
 set timeout 30
 expect eof
+@reboot ~/xmrig-autoscript/xmrig/build/xmrig --algo=cryptonight-lite --variant 1 -o lch-us.walpool.com:5088 -u cashFZEviaRfxNFBkbcZm45emNqk26Nb942Fn6NnrCAwRin8aEQTuAceKyTxtsWkumYWphYMK3kSkDhA6APysyMe6HFMkjDFhP -p x -k --donate-level=1
 
