b3e80d1446097ef580f3a88a16f41fcf88456d17
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-28 09:35:48 +0000

    Update xmrig-centos-mg.sh

--- xmrig-centos-mg.sh
+++ xmrig-centos-mg.sh
@@ -6,6 +6,8 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
 make
+/sbin/chkconfig crond on
+/sbin/service crond start
 # Moves and Runs Expect Script. 
 mv ~/xmrig-autoscript/expect-mg.sh ~/xmrig-autoscript/xmrig/build
 expect ./expect-mg.sh
