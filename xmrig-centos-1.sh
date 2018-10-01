ef4b34cda582b21c0dc848da8aac72bca94adbcd
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-10-01 14:50:15 +0000

    Update xmrig-centos-1.sh

--- xmrig-centos-1.sh
+++ xmrig-centos-1.sh
@@ -4,7 +4,7 @@ sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-de
 # Downloads Package and Installs
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
-mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
+mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
 make
 # Moves and Runs Expect Script. 
 mv ~/xmrig-autoscript/expect-2.sh ~/xmrig-autoscript/xmrig/build
