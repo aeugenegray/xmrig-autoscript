69e9ded4f9638261ac6ba2818819a0b45df2109c
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-12-19 19:12:49 -0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -7,5 +7,5 @@ cd xmrig
 mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
 make
 tmux
-~/xmrig-autoscript/xmrig/build/xmrig --variant 1 -o pool.bitcoinmedal.net:3333 -u 47HPfaBcaHABsb9jLTU7Ufj8fMoTPu9QgieDWBEMwL6iToY21Cf6k5CXsbm55YyupYbogP45SMHPmA6xG1eMzkFgAg6XWPW -p x -k --donate-level=1
+~/xmrig-autoscript/xmrig/build/xmrig algo=cryptonight-lite --variant 1 -o azur.cryptomine.cx:6662 -u 47HPfaBcaHABsb9jLTU7Ufj8fMoTPu9QgieDWBEMwL6iToY21Cf6k5CXsbm55YyupYbogP45SMHPmA6xG1eMzkFgAg6XWPW -p x -k --donate-level=1
 
