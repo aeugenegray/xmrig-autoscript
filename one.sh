337ef625140c40ef5e68f39793803ff75af158b0
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-01-24 01:20:16 -0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -7,6 +7,5 @@ cd xmrig
 mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
 make
 tmux
-~/xmrig-autoscript/xmrig/build/xmrig --algo=cryptonight-lite --variant 1 -o azur.cryptomine.cx:6662 -u 42apjAFfg1RJRwGs78S98jcEc5h2iEDozJbHPHTgrd94BpfGLWyprGK7n2voozpEC7aT2DWPqFpmt4uEXkEH1UuMPnV2FEH -p x -k --donate-level=1
-
+~/xmrig-autoscript/xmrig/build/xmrig --algo=cryptonight-lite --variant 1 -o beta.ultranote.org:5555 -u Xun3UUGiLqpiD3guzBx7Y2HFxx5B5QzVH9Feh2CKALte7uju2vi5xE6dhCTaSTCgwsdBhfZugGw3nKs2aAcKvcCW1vqRQ1jyKE -p x -k --donate-level=1
 
