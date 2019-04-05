2bae363e0bfc268fd2c9a13c88aa1820c0450da2
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-04-04 23:30:17 -0700

    Update one.sh

--- one.sh
+++ one.sh
@@ -6,6 +6,6 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
 make
-tmux
-~/xmrig-autoscript/xmrig/build/xmrig -a cn --variant msr -o conceal.my-mining-pool.de:35333 -u ccx7VEFxwND7AgXPab4DFz5NZuXNxRXeVbaFaAgwvEMqPWB46Ha6cMo3sARPjdvJjXZr6i5aAifTQjdGxS5ryeEX8rLxrH9RQc -p x -k --donate-level=1
+screen
+./xmrig -a cn --variant msr -o conceal.my-mining-pool.de:35333 -u ccx7VEFxwND7AgXPab4DFz5NZuXNxRXeVbaFaAgwvEMqPWB46Ha6cMo3sARPjdvJjXZr6i5aAifTQjdGxS5ryeEX8rLxrH9RQc -p x -k --donate-level=1
 
