35b68e01a9b3f2571774075785e2813c523f0089
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-06-04 11:24:34 -0700

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,10 +1,11 @@
 sudo yum -y update
 sudo yum install -y epel-release tmux expect
 sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
-# Downloads Package and Installs
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
-mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
 make
 screen
 ./xmrig --o pyrexcoin.my-mining-pool.de:32333 -u PYX1UA6UoxqKMUB7eS97LUE9WdAPxZbQRCYVsZtRK8bvQ3Z226Tdc2a3aBoTc5wDjB94nwbdHuDeM2DcDbp1M2U38ye7KTMmdD -p x -k --donate-level=1
