b6cd181ae561d64c0d8b424e3f3a156755cef9ca
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-06-04 11:31:57 -0700

    Update one.sh

--- one.sh
+++ one.sh
@@ -9,3 +9,4 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=O
 make
 screen
 ./xmrig-notls -o pyrexcoin.my-mining-pool.de:32333 -u PYX1UA6UoxqKMUB7eS97LUE9WdAPxZbQRCYVsZtRK8bvQ3Z226Tdc2a3aBoTc5wDjB94nwbdHuDeM2DcDbp1M2U38ye7KTMmdD -p x -k --donate-level=1
+
