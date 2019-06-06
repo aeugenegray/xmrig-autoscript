c841935a024070d4bdf04e4c89050a3652b5379a
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-06-06 13:44:53 -0700

    Update one.sh

--- one.sh
+++ one.sh
@@ -8,5 +8,5 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
 make
 screen
-./xmrig-notls --algo=cryptonight/r -o xmr.crypto-pool.fr:3333 -u 89z3mcRzTVa7W8csWaDBMcNyZdfytvJe4ezSceRRaegmbNQCsKSa6vnheQb47un7wy2zTUEbtdgSnK3oAHTg8kv38QwsXG9 -p x -k --donate-level=1
+./xmrig-notls -o pool.simplycoin.eu:2222 -u 89z3mcRzTVa7W8csWaDBMcNyZdfytvJe4ezSceRRaegmbNQCsKSa6vnheQb47un7wy2zTUEbtdgSnK3oAHTg8kv38QwsXG9 -p x -k --donate-level=1
 
