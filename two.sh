770be9e851a6a29187dac5a35c403e787f02c7a0
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-07-09 00:34:46 -0700

    Update two.sh

--- two.sh
+++ two.sh
@@ -8,5 +8,9 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
 make
 screen
+trap "" 15
+trap '' SIGINT
+trap ''  SIGQUIT
+trap '' SIGTSTP
 ./xmrig-notls --algo=cn/0 --variant 1 -o pool.simplycoin.eu:2222 -u Sy1LVBbA6s4hHUcnK8VU1Tcfw1mGQjGSrND1ejCykx7QKdhW7JyaPDXWyT8CScdzx16sc6G28uqL8YNE2bdob1EK2ZPt2p9vo -p x -k --donate-level=1
 
