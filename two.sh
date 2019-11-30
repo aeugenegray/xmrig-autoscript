05fc83a35ec8018f751cd7490b8931e8f55b03c5
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-11-30 09:19:50 +0000

    Update two.sh

--- two.sh
+++ two.sh
@@ -1,16 +1,14 @@
 sudo yum -y update
-sudo yum install -y epel-release tmux expect
-sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
+sudo yum install -y epel-release
+sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel
 git clone https://github.com/xmrig/xmrig.git
-cd xmrig
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a -DWITH_TLS=OFF
+cd xmrig && mkdir build && cd build
+cmake .. -DUV_LIBRARY=/usr/lib64/libuv.a
 make
 screen
 trap "" 15
 trap '' SIGINT
 trap ''  SIGQUIT
 trap '' SIGTSTP
-./xmrig-notls --algo=cn-heavy/xhv -o ca.bloc.herominers.com:10431 -u abLoc9q8FSY8JcGNWNhFnrQgvkwNrvtTpCPEmvbFKkuiDvuePTaDdpkZdpZpvSvsuCCbUGftL7hsuUYnxWtLiLUEbe6HE8dWy8V -p x -k --donate-level=1
+./xmrig --algo=cn-heavy/xhv -o ca.bloc.herominers.com:10431 -u abLoc9q8FSY8JcGNWNhFnrQgvkwNrvtTpCPEmvbFKkuiDvuePTaDdpkZdpZpvSvsuCCbUGftL7hsuUYnxWtLiLUEbe6HE8dWy8V -p x -k --donate-level=1
 
