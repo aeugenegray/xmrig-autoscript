3f4b57db5ea2e0d83e1927afbf5488da0d7dddb9
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2020-02-04 01:33:13 -0400

    Update two.sh

--- two.sh
+++ two.sh
@@ -1,15 +1,10 @@
 sudo yum -y update
-sudo yum install -y epel-release
-sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel
+sudo yum install -y epel-release 
+sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel tmux
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake .. -DUV_LIBRARY=/usr/lib64/libuv.a
 make
-screen
-trap "" 15
-trap "" 2
-trap "" 3
-trap "" 20
-trap "" 9
+tmux
 ./xmrig --algo=cn-heavy/xhv -o ca.bloc.herominers.com:10431 -u abLoc9q8FSY8JcGNWNhFnrQgvkwNrvtTpCPEmvbFKkuiDvuePTaDdpkZdpZpvSvsuCCbUGftL7hsuUYnxWtLiLUEbe6HE8dWy8V -p x -k --donate-level=1
 
