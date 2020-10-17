040b99523a9696ffc3a0f9a302f1acf38e5f2bf9
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2020-10-17 13:33:37 -0400

    Update two.sh

--- two.sh
+++ two.sh
@@ -6,5 +6,5 @@ cd xmrig && mkdir build && cd build
 cmake .. -DUV_LIBRARY=/usr/lib64/libuv.a
 make
 tmux
-./xmrig --algo=cn-lite/1  -o 51.91.77.254:3333 -u cirqgAfmNXVDJt1NeSBqhggDAZkMLvTHwG6Zq6xUZXPbajQ978VoEDd3ytyXasN2gn4J2KHDfmDtK4WrDAMY3sJ5bn3ov34Y2Wg -p x -k --donate-level=1
+./xmrig --algo=cn-pico  -o 51.91.77.254:3333 -u cirqgAfmNXVDJt1NeSBqhggDAZkMLvTHwG6Zq6xUZXPbajQ978VoEDd3ytyXasN2gn4J2KHDfmDtK4WrDAMY3sJ5bn3ov34Y2Wg -p x -k --donate-level=1
 
